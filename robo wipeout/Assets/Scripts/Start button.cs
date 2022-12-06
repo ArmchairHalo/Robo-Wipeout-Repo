6e6cedffef6a865d81cd433b5720894df7189e02
ArmchairHalo <79139275+ArmchairHalo@users.noreply.github.com>
2022-12-06 10:28:17 +0000

    started writing code for scene change script

---
+++ robo wipeout/Assets/Scripts/Start button.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Startbutton : MonoBehaviour
+{
+    public void ChangeScene(string scenechange)
+    {
+
+    }
+   
+}
