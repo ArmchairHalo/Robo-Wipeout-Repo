a7d1cd5b76c3364683c5cff8a2b10ba6914d951b
ArmchairHalo <79139275+ArmchairHalo@users.noreply.github.com>
2023-01-05 12:46:20 +0000

    created jump function and added gravity to the game

--- robo wipeout/Assets/Scripts/ThirdPersonMovement.cs
+++ robo wipeout/Assets/Scripts/ThirdPersonMovement.cs
@@ -12,17 +12,35 @@ public class ThirdPersonMovement : MonoBehaviour
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    public float jumpHeight = 1.0f;
+    public float gravityValue = -9.81f;
+
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 
+        groundedPlayer = controller.isGrounded;
+        Debug.Log("grounded player =" + groundedPlayer);
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
         string[] joysticks = Input.GetJoystickNames();
+
+        for(int i = 0; i < joysticks.Length; ++i)
+        {
+            //Debug.Log(i+" "+joysticks[i]);
+        }
 
 
         float horizontal = Input.GetAxis("HorizontalL");
         float vertical = Input.GetAxis("VerticalL");
 
-        Debug.Log(horizontal + ", " + vertical);
+        //Debug.Log(horizontal + ", " + vertical);
 
         Vector3 direction = new Vector3(horizontal, 0f, vertical);
 
@@ -36,5 +54,19 @@ public class ThirdPersonMovement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir * speed * Time.deltaTime);
         }
+
+        if (Input.GetButton("Jump"))
+        {
+            Debug.Log("JUMP PRESSED!!!!!");
+        }
+
+        if (Input.GetButton("Jump") && groundedPlayer)
+        {
+            Debug.Log("JUMP PRESSED AND GROUNDED!!!!!!");
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
