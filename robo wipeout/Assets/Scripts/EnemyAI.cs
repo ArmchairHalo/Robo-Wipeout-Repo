60d3608ee4baaa8157a0b59a0a9c669a7583ad32
ArmchairHalo <79139275+ArmchairHalo@users.noreply.github.com>
2023-01-14 00:46:05 +0000

    got enemy AI working correctly (need to test that it works with my created assets and not just with test assets)

--- robo wipeout/Assets/Scripts/EnemyAI.cs
+++ robo wipeout/Assets/Scripts/EnemyAI.cs
@@ -5,21 +5,21 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    
     NavMeshAgent agent;
     public Transform[] waypoints;
     int waypointIndex;
     Vector3 target;
+    // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         UpdateDestination();
     }
 
-    
+    // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, target) < 1)
+        if(Vector3.Distance(transform.position, target) < 1)
         {
             IterateWaypointIndex();
             UpdateDestination();
