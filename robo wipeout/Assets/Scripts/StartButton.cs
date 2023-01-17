aa79c5767ca39d4bc03ebe2ace29081faa60f9bf
ArmchairHalo <79139275+ArmchairHalo@users.noreply.github.com>
2023-01-17 11:06:06 +0000

    made it so that the buttons can be used by the controller and are highlighted to show what button will be pressed

--- robo wipeout/Assets/Scripts/StartButton.cs
+++ robo wipeout/Assets/Scripts/StartButton.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartButton : MonoBehaviour
 {
+
+    void Start()
+    {
+        GetComponent<Button>().Select();
+    }
+
     public void ChangeScene(string scenechange)
     {
         SceneManager.LoadScene(scenechange);
